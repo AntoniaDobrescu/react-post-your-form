8bbbd74637bba0f00459795066a6ce3dfd8aae75
AntoniaDobrescu <antonia.dobrescu91@gmail.com>
2019-11-18 10:15:42 +0200

    Updates

---
+++ precache-manifest.916d115db73f865e175afb8f9258311e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29c6c336d10f538db90bce4033a0252",
+    "url": "/react-post-your-form/index.html"
+  },
+  {
+    "revision": "73cadcf6b914535f71e3",
+    "url": "/react-post-your-form/static/css/main.b1950d23.chunk.css"
+  },
+  {
+    "revision": "da3a8b708e8afd02e4a2",
+    "url": "/react-post-your-form/static/js/2.2dc62986.chunk.js"
+  },
+  {
+    "revision": "73cadcf6b914535f71e3",
+    "url": "/react-post-your-form/static/js/main.34946099.chunk.js"
+  },
+  {
+    "revision": "212f54f3f0b7f0503b20",
+    "url": "/react-post-your-form/static/js/runtime-main.4a2a711b.js"
+  }
+]);
